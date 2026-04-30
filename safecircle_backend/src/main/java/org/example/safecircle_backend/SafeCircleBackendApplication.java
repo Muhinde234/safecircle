36f5390153ad8f580f97166ecbe3dec804cd1bd5
Nicole0509 <niashavril0509@gmail.com>
2026-04-30 14:52:24 +0200

    Successfully created a session with error handling and fallback logic

--- safecircle_backend/src/main/java/org/example/safecircle_backend/SafeCircleBackendApplication.java
+++ safecircle_backend/src/main/java/org/example/safecircle_backend/SafeCircleBackendApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SafeCircleBackendApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(SafeCircleBackendApplication.class, args);
     }
 
--- safecircle_backend/src/main/java/org/example/safecircle_backend/session/controller/SessionController.java
+++ safecircle_backend/src/main/java/org/example/safecircle_backend/session/controller/SessionController.java
@@ -1,4 +1,28 @@
 package org.example.safecircle_backend.session.controller;
 
+import org.example.safecircle_backend.session.dto.CreateSessionRequest;
+import org.example.safecircle_backend.session.dto.SessionResponse;
+import org.example.safecircle_backend.session.service.SessionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/sessions")
 public class SessionController {
+
+    private final SessionService sessionService;
+
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @PostMapping("/anonymous")
+    public ResponseEntity<SessionResponse> createAnonymousSession(@RequestBody(required = false) CreateSessionRequest request) {
+        if(request == null){
+            request = new CreateSessionRequest();
+        }
+
+        return new ResponseEntity<>(sessionService.createAnonymousSession(request), HttpStatus.CREATED);
+    }
 }
